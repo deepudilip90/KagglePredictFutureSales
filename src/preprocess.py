dae1d93342821748c6a6ee4c3d5d8e46af64433f
Deepu Dilip <deepud90@vt.edu>
2021-06-06 13:54:27 +0200

    working code for preprocessing pipeline Jupyter notebook for preprocessing data

--- src/preprocess.py
+++ src/preprocess.py
@@ -127,30 +127,29 @@ def prepare_train_df(sales_train_df, train_months=34):
     return data_matrix
 
 
-def write_prepared_data(train_df, valid_df, test_df):
+def clean_and_write_prepared_data(data):
     """
     Convenience method to write pre-processed train and test data to disk, with appropriate names
-    :param train_df:
-    :param test_df:
+    :param data:
     :return:
     """
-    feature_count_train = len(train_df.columns) - 2
-    feature_count_test = len(test_df.columns) - 1
+    data = fill_null_for_lag_cols(data)
 
-    if feature_count_train != feature_count_test:
-        print('Error: unequal count of features in train and test set ')
-        train_df.to_pickle(PROCESSED_DATA_PATH, 'train_df_temp.pkl')
-        test_df.to_pickle(PROCESSED_DATA_PATH, 'test_df_temp.pkl')
-        return False
+    # train data
+    train_data = data[(data['date_block_num'] > 11) & (data['date_block_num'] < 33)]
+    valid_data = data[data['date_block_num'] == 33]
+    test_data = data[data['date_block_num'] == 34]
+
+    feature_count = len(train_data.columns) - 2
 
     file_name_postfix = str(date.today()) + '.pkl'
-    train_file_name = 'train_df_' + str(feature_count_train) + '_features_' + file_name_postfix
-    valid_file_name = 'valid_df_' + str(feature_count_train) + '_features_' + file_name_postfix
-    test_file_name = 'test_df' + str(feature_count_test) + '_features_' + file_name_postfix
+    train_file_name = 'train_df_' + str(feature_count) + '_features_' + file_name_postfix
+    valid_file_name = 'valid_df_' + str(feature_count) + '_features_' + file_name_postfix
+    test_file_name = 'test_df_' + str(feature_count) + '_features_' + file_name_postfix
 
-    train_df.to_pickle(PROCESSED_DATA_PATH, train_file_name)
-    valid_df.to_pickle(PROCESSED_DATA_PATH, valid_file_name)
-    test_df.to_pickle(PROCESSED_DATA_PATH, test_file_name)
+    train_data.to_pickle(path.join(PROCESSED_DATA_PATH, train_file_name))
+    valid_data.to_pickle(path.join(PROCESSED_DATA_PATH, valid_file_name))
+    test_data.to_pickle(path.join(PROCESSED_DATA_PATH, test_file_name))
 
     return True
 
@@ -192,9 +191,6 @@ def merge_dataframes(sales_train_df, item_df, item_cat_df, shops_df, test_df):
     test_df = test_df.merge(item_df, on='item_id', how='left')
     test_df = test_df.merge(item_cat_df, on='item_category_id', how='left')
 
-    # train_df['is_train'] = 1
-    # test_df['is_train'] = 0
-
     combined_df = pd.concat([train_df, test_df])
 
     return combined_df
@@ -232,9 +228,19 @@ def precheck_for_run_step(data, run_step_no, read_step):
     return True
 
 
+def fill_null_for_lag_cols(data):
+
+    lagged_columns_with_null = [col for col in data.columns
+                                if 'lagged' in col and data[col].isnull().any()]
+    for col in lagged_columns_with_null:
+        data[col].fillna(0, inplace=True)
+
+    return data
+
+
 def main(run_config):
     """
-    Main method to run the preprocessing steps
+    Main method to run the preprocessing steps.
     :return:
     """
     read_step = run_config.get('read_step', 0)
@@ -244,12 +250,14 @@ def main(run_config):
     run_step_3 = run_config.get('run_step_3', True)
     run_step_4 = run_config.get('run_step_4', True)
     run_step_5 = run_config.get('run_step_5', True)
+    run_step_6 = run_config.get('run_step_6', True)
 
     write_step_1 = run_config.get('write_step_1', False)
     write_step_2 = run_config.get('write_step_2', False)
     write_step_3 = run_config.get('write_step_3', False)
     write_step_4 = run_config.get('write_step_4', False)
     write_step_5 = run_config.get('write_step_5', False)
+    write_step_6 = run_config.get('write_step_6', False)
 
     all_data = read_all_data(DATA_PATH)
 
@@ -318,30 +326,29 @@ def main(run_config):
         if write_step_5:
             write_restart(preprocessed_data, 5)
 
-    # step tp clean data
+    # clean and write preprocessed data
+    if run_step_6:
+        print('running step 6')
+        if not precheck_for_run_step(preprocessed_data, 6, read_step):
+            return False
 
-    # train data
-    # train_data = preprocessed_data[(preprocessed_data['date_block_num'] > 11) &
-    #                              (preprocessed_data['date_block_num'] < 33)]
-    # valid_data = preprocessed_data[preprocessed_data['date_block_num'] == 33]
-    # test_data = preprocessed_data[preprocessed_data['date_block_num'] == 34]
-    #
-    # print('writing output')
-    # write_prepared_data(train_data, valid_data, test_df)
+        print('cleaning and writing train, test and validation data')
+        clean_and_write_prepared_data(preprocessed_data)
 
 
 if __name__ == '__main__':
-    run_config = {'read_step': 0,
-                  'run_step_1': True,
-                  'run_step_2': True,
-                  'run_step_3': True,
-                  'run_step_4': True,
-                  'run_step_5': True,
-                  'write_step_1': True,
-                  'write_step_2': True,
-                  'write_step_3': True,
-                  'write_step_4': True,
-                  'write_step_5': True}
+    run_config = {'read_step': 5,
+                  'run_step_1': False,
+                  'run_step_2': False,
+                  'run_step_3': False,
+                  'run_step_4': False,
+                  'run_step_5': False,
+                  'run_step_6': True,
+                  'write_step_1': False,
+                  'write_step_2': False,
+                  'write_step_3': False,
+                  'write_step_4': False,
+                  'write_step_5': False}
     main(run_config)
 
 
